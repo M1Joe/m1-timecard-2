9b40b99e9c97da919024ef9e5355eb7d94bb2816
joe <joe@m1technology.com>
2019-06-15 15:22:02 -0400

    Initial Commit

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseKeys = {
+  // For your own projects please use different keys
+  apiKey: 'AIzaSyC_j54Tztk7u3CyEIOgu_qvvKV3Gg0jW2M',
+  authDomain: 'm1-timecard-2.firebaseapp.com',
+  databaseURL: 'https://m1-timecard-2.firebaseio.com',
+  projectId: 'm1-timecard-2',
+  storageBucket: 'm1-timecard-2.appspot.com',
+  messagingSenderId: '218619246157'
+}
