9eb97d7984570c77517a34370728ddf8cd55db4a
joe <joe@m1technology.com>
2019-10-15 21:54:10 -0400

    added next-user functionality for admins

--- src/app/pages/timecard/approve-timecard/approve-timecard.component.ts
+++ src/app/pages/timecard/approve-timecard/approve-timecard.component.ts
@@ -18,6 +18,7 @@ export class ApproveTimecardComponent implements OnInit {
 
   //currentUserKey: string; //key of current user that we are viewing/approving
   users$: Observable<User[]>;
+  userArray: User[];
 
   pto: PTO = new PTO();
   
@@ -38,12 +39,16 @@ export class ApproveTimecardComponent implements OnInit {
 
   ngOnInit(): void {
     this.users$ = this.userService.getUsers();
+    
+    this.users$.pipe(take(1)).subscribe(result => this.userArray = result);
+
     this.userService.getOfficialPto(this.authService.getUserKey(this.currentUser)).pipe(take(1)).subscribe(oldPto => {
       this.oldAsOf = oldPto.asOf;
       this.oldBalance = oldPto.balance;
     });
 
-    console.log('pto' + this.totalHoursPTO);
+
+
   }
 
   approveTimecard() {
@@ -60,6 +65,9 @@ export class ApproveTimecardComponent implements OnInit {
     this.requestToSaveTimecard.emit('REJECTED');
   }
 
+
+  // These two functions do the same thing, but one fires on selectionChange of the mat-select and the other on the "Next" button
+  // They take different inputs.  These are a copy/paste of each other, which is wrong, but it works.
   loadTimecardFor(event) {
     this.requestToLoadTimecardForUser.emit(event.value);
     
@@ -71,9 +79,21 @@ export class ApproveTimecardComponent implements OnInit {
         this.oldAsOf = oldPto.asOf;
         this.oldBalance = oldPto.balance;
       }
-      
     });
+  }
+
+  loadTimecardForUser(user) {
+    this.requestToLoadTimecardForUser.emit(user);
     
+    this.oldAsOf = 'UNKNOWN';
+    this.oldBalance = '-999';
+
+    this.userService.getOfficialPto(this.authService.getUserKey(user)).pipe(take(1)).subscribe(oldPto => {
+      if (oldPto) {
+        this.oldAsOf = oldPto.asOf;
+        this.oldBalance = oldPto.balance;
+      }
+    });
   }
 
   compareObjects(user1: User, user2: User): boolean {
@@ -88,5 +108,23 @@ export class ApproveTimecardComponent implements OnInit {
   calculatePto() {
     var newBalance = +this.oldBalance + 11.67 - this.totalHoursPTO;
     return newBalance.toFixed(2);
+  }
+
+  viewNextUser() {
+    // This function allows an admin to view the next user's timecard.  
+    // This is totally the wrong way to do this, but it works.
+    let counter = 0;
+    for (let user of this.userArray) {
+      if (this.currentUser.email === user.email) {
+        if (counter === this.userArray.length - 1) {
+          this.currentUser = this.userArray[0];
+        } else {
+          this.currentUser = this.userArray[counter + 1];
+        }   
+        this.loadTimecardForUser(this.currentUser);
+        return;
+      }
+      counter++;
+    }
   }
 }
